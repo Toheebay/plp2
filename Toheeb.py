b8efb66e29576f4b74b6cda10ad88ae22c011045
Toheebay <successphu@gmail.com>
2025-02-28 06:15:30 -0800

    Updated feature or bug fix

--- Toheeb.py
+++ Toheeb.py
@@ -1,10 +1,13 @@
-marks=100
-if marks >70:
+# marks=100
+# if marks >70:
 
-    print("Very Excellent")
+#     print("Very Excellent")
 
-elif  marks >= 60:
-    print("Very good Result")
+# elif  marks >= 60:
+#     print("Very good Result")
 
-else:
-   print ("You can try again")
+# else:
+#    print ("You can try again")
+
+
+print('here is a double quote"'+ " here is single quote'")
---
+++ practise.py
@@ -0,0 +1,58 @@
+# a=[-1, -7, 0,0,"this text",10]
+# print (a)
+# a[2]= "Sal ' s string"
+# print(a)
+# b = a
+# print(b)
+# c= a[2:]
+# a.append("new element")
+# a.append(11)
+# a.append(12)
+# a.append(13)
+# a.append(14)
+# a.append(15)
+# a.append(16)
+# a.remove(16)
+# print(a)
+# a=range(2)
+# print(a)
+
+# answer =input("Do you like express shipping ?")
+# if answer.lower() != "no":
+#  print("That will be an extral $20")
+# else:
+#  print("Ko do min")
+
+# import datetime
+# CurrentDate = datetime.date.today()
+# print(CurrentDate)
+# print(CurrentDate.year)
+# print(CurrentDate.month)
+
+# import random
+# msg = "roll a dice"
+# print(msg)
+
+# import numpy as np
+# import random
+# view="Let learn blog using canva ABCD Learning techniques"
+# print(random.randint(2, 20))
+# print(view)
+  
+# sodiq= False
+# if sodiq == True:
+#     print("We will learn with him")
+# else:
+#     print("Let google and search for tutor")
+country=input("what is your country ?")
+province=input("what is your province in canada?")
+order=input(" what is your order in total?")
+if province == "antorio": 
+    print("charge 0.5% General Sales Tax (GST) ")
+elif province == "Albeta" or " novascoxia":
+    print("charge .13% Harmonized Sales Tax")
+elif province=="Antorio" or "Others":
+    print("charge .13% harmonized Sales Tax")
+else:
+    print("charge .06% to other province +0.5% tax")
+    
